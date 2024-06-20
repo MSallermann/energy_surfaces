85362c88e2e0b8564fcc7b0238d5053e640d6149
Moritz Sallermann <moritzsallermann@gmail.com>
2024-06-20 14:03:56 +0000

    Initial commit. Separated energy_surfaces from ridgefollowing repository

--- mwe.py
+++ mwe.py
@@ -1,14 +1,3 @@
-# Energy_surfaces
-Some energy surfaces. Implemented in python with numba JIT
-
-# Installation
-
-```bash
-pip install -e .
-```
-
-# MWE
-```python
 from energy_surfaces.surfaces import lepshogauss
 import numpy as np
 
@@ -18,4 +7,3 @@ x = np.array([2.1, 1.2])
 print(f"Energy = {esurf.energy(x)}")
 print(f"Gradient = {esurf.gradient(x)}")
 print(f"Hessian = {esurf.hessian(x)}")
-```
